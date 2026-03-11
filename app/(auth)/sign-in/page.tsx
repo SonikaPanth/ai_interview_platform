0fd08b3832a86506c33818b3ce076a012f8e0b4a
SonikaPanth <sonikapanth@gmail.com>
2026-03-11 18:36:38 +0530

    fix: removed eslint warning

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import AuthForm from '@/app/component/authform'
 import React from 'react'
 
+export const dynamic = "force-dynamic";
 const SignInPage = () => {
   return <AuthForm type="sign-in"/>
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AuthForm from '@/app/component/authform'
 
+export const dynamic = "force-dynamic";
 const SignupPage = ({children}:{children: React.ReactNode}) => {
    return <AuthForm type="sign-up"/>
 }
